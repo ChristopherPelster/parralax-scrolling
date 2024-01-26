feed5d8837e0ed292134c4a6089c3e0eee20f9b1
Christopher Pelster <christopher.pelster@gmail.com>
2024-01-26 22:38:12 +0100

    fisch scrolling

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+let menu = document.querySelector("#mobile-menu");
+let menuLinks = document.querySelector(".navbar__menu");
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
+
+let background = document.getElementById("sea");
+let fisch1 = document.getElementById("fisch1");
+let textExample1 = document.getElementById("text-example1");
+let scrollHightOld = 0;
+
+window.addEventListener("scroll", () => {
+  let scrollHight = window.scrollY;
+  let windowWidth = window.innerWidth;
+  let windowHight = window.innerHeight;
+
+  let parallaxStay = scrollHight + "px";
+  let parallaxMoveRight = (scrollHight / windowHight) * windowWidth;
+
+  background.style.marginTop = parallaxStay;
+  if (parallaxMoveRight < windowWidth + 200) {
+    fisch1.style.marginLeft = parallaxMoveRight + "px";
+    fisch1.style.marginTop = parallaxStay;
+    textExample1.style.marginLeft = parallaxMoveRight + "px";
+    textExample1.style.marginTop = parallaxStay;
+  } else if (parallaxMoveRight < windowWidth * 2 + 300) {
+    parallaxMoveRight -= 600
+    fisch1.style.marginLeft = parallaxMoveRight - windowWidth -100 + "px";
+    fisch1.style.marginTop = parallaxStay;
+  } else {
+    
+  }
 });
